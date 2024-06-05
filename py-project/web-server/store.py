9c71418e9a4b669d94c5ef5e2163189bb842a37a
mariajmnz1 <mariajmnzmansilla@gmail.com>
2024-06-05 13:26:48 +0200

    add the new changes with docker use

---
+++ py-project/web-server/store.py
@@ -0,0 +1,10 @@
+import requests
+
+def get_categories():
+    r = requests.get('https://api.escuelajs.co/api/v1/categories')
+    print(r.status_code)
+    print(r.text)
+    print(type(r.text))
+    categories = r.json() #para transformarlo a una list y hacer iteraciones
+    for category in categories:
+        print(category['name'])
